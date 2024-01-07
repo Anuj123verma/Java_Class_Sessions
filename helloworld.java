dc72943d940ba9b1c0e55175ba3de3f2171d3c21
Anuj123verma <anuj17026@iiitd.ac.in>
2024-01-07 22:51:31 +0530

    previous class

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class helloworld{
-    public static int main(person p){
+    public int getPersonAge(person p){
         return p.age;
     }
+    
     public static void main(String[] args){
         // want to take the input
         // new keyword is use for creating object of scanner (in general any class)
@@ -101,6 +102,118 @@ public class helloworld{
         // i=3
         // i=4
         // int z = x*++i; // 20
+
+
+        // primitives int byte short long float double char boolean
+        // generic person string car animal list stack queue
+
+        // int x = 5
+        // long y = 9
+        // int z = x*y  --> incorrect
+        // long z = x*y --> that is the correct thing
+
+        // int x = 5;
+        // String greetings = "hello";
+        // reference variables -> stores the address of the object i.e "hello"
+
+        // String str = "abc";
+        // String str = new String("abc");
+        // new keyword is used to create the instance of the class i.e Object
+
+        // int[]  x = {1,2,3};
+        // for(int y : x){
+        //     System.out.println(y);
+        // }
+        // 1
+        // 2
+        // 3
+        // int len = x.length;
+        // for(int i=0;i<length;i++){
+        //     System.out.println(x[i]);
+        // }
+        // 1
+        // 2
+        // 3
+
+        for(int nextInt =1;nextInt<=maxVal; nextInt++){
+            if(nextInt%2==0){
+                sum+=nextInt;
+            }
+            else{
+                prod*=nextInt;
+            }
+        }
+        // initializaton
+        // condition
+        // increment
+        int nextInt=1;
+        do{
+            if(nextInt%2==0){
+                sum+=nextInt;
+            }
+            else{
+                prod*=nextInt;
+            }
+        }
+        while(/*condition*/)
+        
+        int nextInt =1;
+        // maxVal = 10
+        while(nextInt<=maxVal){
+            if(nextInt%2==0){
+                sum+=nextInt;
+            }
+            else{
+                prod*=nextInt;
+            }
+            nextInt++;
+        }
+
+// first type
+        if(condition){
+
+        }
+        else{
+
+        }
+
+
+        if(condition){
+
+        }
+        else if(condition){
+
+        }
+        else if(condition){
+
+        }
+
+        if(condition){  --> true
+            // enter inside this block
+        } // this will execute
+        if(condition){
+            // enter inside this block
+        }
+        if(condition){
+
+        }
+
+        int nextInt =1;
+        int maxVal=0;
+        // after first execution of do block it will go to while condition
+        do{
+            // code for execution
+            nextInt++;
+        }
+        while(nextInt<=maxVal);
+
+
+
+        <access modifier> <modifier> <return type> <function name>(args){
+            // operation
+            // return // whatever type it has u ll return it
+            // void --> nothing to retur
+        }
     }
 }
 
@@ -114,4 +227,14 @@ class Pairs{
         person p = new person();
         System.out.println(p.age);
     }
+
+    // static method is the class method , you can directly call for the class
+    // you do not have to make the object of that class
+    static int findDistance(Pairs p1, Pairs p2){
+        // find the distane of the two pairs
+        // retunr something that has int value
+        // modulus distance of two points
+    }
+
+    int distance = Pairs.findDistance();
 }
